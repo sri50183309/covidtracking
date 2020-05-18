792a5f36011afcfd8ea663fe5d8e80dca79ee83a
Sriram <sriram.mahalingam@gmail.com>
2020-05-18 06:57:24 -0400

    added tabs and graphs , removed the real http code

--- lib/main.dart
+++ lib/main.dart
@@ -1,72 +1,19 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-import 'dart:async';
-import 'dart:convert';
+import 'homepage.dart';
 
-void main() {
-  runApp(MaterialApp(
-    home: HomePage(),
-  ));
-}
+void main() => runApp(MyApp());
 
-class HomePage extends StatefulWidget {
-  @override
-  _HomePageState createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  Map data;
-  List userData;
-
-  Future getData() async {
-    http.Response response = await http.get(
-        "https://corona-virus-stats.herokuapp.com/api/v1/cases/countries-search?limit=20&page=1");
-    data = json.decode(response.body);
-    setState(() {
-      userData = data["data"]["rows"];
-    });
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    getData();
-  }
-
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Fake Friends"),
-        backgroundColor: Colors.green,
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'COVID Tracking',
+      theme: ThemeData(
+        primaryColor: Color(0xffff6101),
       ),
-      body: ListView.builder(
-        itemCount: userData == null ? 0 : userData.length,
-        itemBuilder: (BuildContext context, int index) {
-          return Card(
-            child: Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: Row(
-                children: <Widget>[
-                  CircleAvatar(
-                    backgroundImage: NetworkImage(userData[index]["flag"]),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(10.0),
-                    child: Text(
-                      "${userData[index]["country"]} ${userData[index]["total_cases"]}",
-                      style: TextStyle(
-                        fontSize: 20.0,
-                        fontWeight: FontWeight.w700,
-                      ),
-                    ),
-                  )
-                ],
-              ),
-            ),
-          );
-        },
-      ),
+      home: HomePage(),
     );
   }
 }
