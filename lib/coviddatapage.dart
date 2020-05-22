dacad5206aedc8463b19b0be6e1b9a251f076b0a
Sriram <sriram.mahalingam@gmail.com>
2020-05-22 07:18:37 -0400

    added bottom bar, share and rate app icon

--- lib/coviddatapage.dart
+++ lib/coviddatapage.dart
@@ -38,19 +38,39 @@ class _COVIDDataPageState extends State<COVIDDataPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-          child: FutureBuilder(
-        future: Future.wait([getWorldStatistics(), getIndiaStatistics()]),
-        builder: (context, AsyncSnapshot<List> snapshot) {
-          if (!snapshot.hasData) {
-            return CircularProgressIndicator(
-              backgroundColor: Colors.cyanAccent,
-              valueColor: new AlwaysStoppedAnimation<Color>(Colors.red),
-            );
-          } else {
-            return DisplayData(snapshot.data[0], snapshot.data[1]);
-          }
-        },
-      )),
+        child: FutureBuilder(
+          future: Future.wait([getWorldStatistics(), getIndiaStatistics()]),
+          builder: (context, AsyncSnapshot<List> snapshot) {
+            if (!snapshot.hasData) {
+              return CircularProgressIndicator(
+                backgroundColor: Colors.cyanAccent,
+                valueColor: new AlwaysStoppedAnimation<Color>(Colors.red),
+              );
+            } else {
+              return DisplayData(snapshot.data[0], snapshot.data[1]);
+            }
+          },
+        ),
+      ),
+      bottomNavigationBar:
+          BottomNavigationBar(type: BottomNavigationBarType.fixed, items: [
+        BottomNavigationBarItem(
+          icon: Icon(
+            Icons.share,
+            color: Colors.purple,
+          ),
+          title: Text(
+            "Share",
+            style: TextStyle(fontStyle: FontStyle.italic, color: Colors.purple),
+          ),
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.rate_review, color: Colors.purple),
+          title: Text("Rate App",
+              style:
+                  TextStyle(fontStyle: FontStyle.italic, color: Colors.purple)),
+        )
+      ]),
     );
   }
 }
