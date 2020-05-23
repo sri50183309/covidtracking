04badb41f6a8d7c88a70bf3e0be48ffdbd204d3a
Sriram <sriram.mahalingam@gmail.com>
2020-05-23 18:05:00 -0400

    added covid by world in to menu

--- lib/pages/my_cards_page.dart
+++ lib/pages/my_cards_page.dart
@@ -1,5 +1,6 @@
 import 'package:covidtracker/bloc/navigation_bloc/navigation_bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class MyCardsPage extends StatelessWidget with NavigationStates {
   final Function onMenuTap;
@@ -8,23 +9,21 @@ class MyCardsPage extends StatelessWidget with NavigationStates {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.all(Radius.circular(40)),
-        color: Colors.grey,
-      ),
-      child: SingleChildScrollView(
-        scrollDirection: Axis.vertical,
-        physics: ClampingScrollPhysics(),
-        child: Container(
+    return DefaultTabController(
+      length: 2,
+      initialIndex: 1,
+      child: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.all(Radius.circular(40)),
+            color: Colors.redAccent,
+          ),
           padding: const EdgeInsets.only(left: 16, right: 16, top: 48),
           child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 mainAxisSize: MainAxisSize.max,
-                children: [
+                children: <Widget>[
                   InkWell(
                     child: Icon(Icons.menu, color: Colors.white),
                     onTap: onMenuTap,
@@ -34,36 +33,25 @@ class MyCardsPage extends StatelessWidget with NavigationStates {
                   Icon(Icons.settings, color: Colors.white),
                 ],
               ),
-              SizedBox(height: 50),
-              Container(
-                height: 200,
-                child: PageView(
-                  controller: PageController(viewportFraction: 0.8),
-                  scrollDirection: Axis.horizontal,
-                  pageSnapping: true,
-                  children: <Widget>[
-                    Container(
-                      margin: const EdgeInsets.symmetric(horizontal: 8),
-                      color: Colors.redAccent,
-                      width: 100,
-                    ),
-                    Container(
-                      margin: const EdgeInsets.symmetric(horizontal: 8),
-                      color: Colors.blueAccent,
-                      width: 100,
-                    ),
-                    Container(
-                      margin: const EdgeInsets.symmetric(horizontal: 8),
-                      color: Colors.greenAccent,
-                      width: 100,
-                    ),
-                  ],
-                ),
-              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                mainAxisSize: MainAxisSize.max,
+                children: <Widget>[
+                  TabBar(
+                    indicatorColor: Color(0xff9962D0),
+                    tabs: [
+                      Tab(
+                        icon: Icon(FontAwesomeIcons.solidChartBar),
+                      ),
+                      Tab(
+                        icon: Icon(FontAwesomeIcons.globe),
+                      ),
+                    ],
+                  ),
+                ],
+              )
             ],
-          ),
-        ),
-      ),
+          )),
     );
   }
 }
