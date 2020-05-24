1f88ccac6181e74d7f400eea1e7aa421da7d2869
Sriram <sriram.mahalingam@gmail.com>
2020-05-24 07:07:44 -0400

    in world page, added graph

--- lib/pages/my_cards_page.dart
+++ lib/pages/my_cards_page.dart
@@ -1,10 +1,22 @@
 import 'package:covidtracker/bloc/navigation_bloc/navigation_bloc.dart';
+import 'package:covidtracker/model/ByCountry.dart';
+import 'package:covidtracker/model/Constants.dart';
 import 'package:flutter/material.dart';
+
+import 'package:charts_flutter/flutter.dart' as charts;
+import 'package:http/http.dart' as http;
+import 'dart:async';
+import 'dart:convert';
 
 class MyCardsPage extends StatelessWidget with NavigationStates {
   final Function onMenuTap;
 
   const MyCardsPage({Key key, this.onMenuTap}) : super(key: key);
+
+  Future<List> getWorldStatistics() async {
+    http.Response response = await http.get(Constants.COVID_STATS_IN_WORLD);
+    return COVIDByCountry.byTotalCases(json.decode(response.body));
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -29,14 +41,14 @@ class MyCardsPage extends StatelessWidget with NavigationStates {
                     child: Icon(Icons.menu, color: Colors.white),
                     onTap: onMenuTap,
                   ),
-                  Text("My Cards",
+                  Text("COVID around the world",
                       style: TextStyle(fontSize: 24, color: Colors.white)),
                   Icon(Icons.settings, color: Colors.white),
                 ],
               ),
               SizedBox(height: 50),
               Container(
-                height: 200,
+                height: 300,
                 child: PageView(
                   controller: PageController(viewportFraction: 0.8),
                   scrollDirection: Axis.horizontal,
@@ -44,30 +56,40 @@ class MyCardsPage extends StatelessWidget with NavigationStates {
                   children: <Widget>[
                     Container(
                       margin: const EdgeInsets.symmetric(horizontal: 8),
-                      color: Colors.redAccent,
+                      color: Colors.white70,
                       width: 100,
-                    ),
-                    Container(
-                      margin: const EdgeInsets.symmetric(horizontal: 8),
-                      color: Colors.blueAccent,
-                      width: 100,
-                    ),
-                    Container(
-                      margin: const EdgeInsets.symmetric(horizontal: 8),
-                      color: Colors.greenAccent,
-                      width: 100,
+                      child: FutureBuilder(
+                        future: Future.wait([getWorldStatistics()]),
+                        builder: (context, AsyncSnapshot<List> snapshot) {
+                          if (!snapshot.hasData) {
+                            return CircularProgressIndicator(
+                              backgroundColor: Colors.cyanAccent,
+                              valueColor:
+                                  new AlwaysStoppedAnimation<Color>(Colors.red),
+                            );
+                          } else {
+                            List<COVIDByCountry> covidInWorld =
+                                snapshot.data[0];
+                            List<charts.Series<COVIDByCountry, String>>
+                                _covidByCountry =
+                                generateGraphData(covidInWorld);
+
+                            return renderGraphInUI(_covidByCountry);
+                          }
+                        },
+                      ),
                     ),
                   ],
                 ),
               ),
               SizedBox(height: 20),
               Text(
-                "Transactions",
+                "Data in Numbers",
                 style: TextStyle(color: Colors.white, fontSize: 20),
               ),
               ListView.separated(
                 shrinkWrap: true,
-                itemBuilder: (context, index) {
+                itemBuilder: (covi, index) {
                   return ListTile(
                     title: Text("Macbook"),
                     subtitle: Text("Apple"),
@@ -85,5 +107,39 @@ class MyCardsPage extends StatelessWidget with NavigationStates {
         ),
       ),
     );
+  }
+
+  Column renderGraphInUI(
+      List<charts.Series<COVIDByCountry, String>> _covidByCountry) {
+    return Column(
+      children: <Widget>[
+        Expanded(
+          child: charts.BarChart(
+            _covidByCountry,
+            animate: true,
+            barGroupingType: charts.BarGroupingType.grouped,
+            //behaviors: [new charts.SeriesLegend()],
+            animationDuration: Duration(seconds: 5),
+          ),
+        ),
+      ],
+    );
+  }
+
+  List<charts.Series<COVIDByCountry, String>> generateGraphData(
+      List<COVIDByCountry> covidInWholeWolrd) {
+    var covidByTotalCases = covidInWholeWolrd;
+    return [
+      charts.Series(
+        domainFn: (COVIDByCountry covidInWorld, _) =>
+            covidInWorld.country_abbreviation,
+        measureFn: (COVIDByCountry covidInWorld, _) => covidInWorld.total_cases,
+        id: '1',
+        data: covidByTotalCases,
+        fillPatternFn: (_, __) => charts.FillPatternType.solid,
+        fillColorFn: (COVIDByCountry covidInWorld, _) =>
+            charts.ColorUtil.fromDartColor(covidInWorld.colorval),
+      ),
+    ];
   }
 }
